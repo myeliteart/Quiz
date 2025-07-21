2b55b848ee119cc90c774fb97b4270b637c69fbf
myeliteart <eliteny0@gmail.com>
2025-07-21 18:50:59 -0400

    update FInalScore

--- src/components/FinalScore.vue
+++ src/components/FinalScore.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="border-y-5 border-amber-800 bg-white mt-5 py-9">
         <div>
-            <h3 class="text-2xl sm:text-3xl md:text-5xl">Congratulations!</h3>
+            <h3 class="text-3xl md:text-5xl">Congratulations!</h3>
             <div class="max-w-[350px] md:max-w-[650px] mx-auto mt-8 font-medium text-amber-800">
                {{ `You had ${correct} answer/s out of ${quizy.question.length} questions correct` }}
             </div>
